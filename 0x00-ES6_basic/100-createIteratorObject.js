63138564b66d991c28c0a4e06c24e61274bf05e4
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-07-26 17:02:04 +0100

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const out = [];
-  const keys = Object.keys(report); // Array
+  const keys = Object.keys(report.allEmployees); // Array
   for (const key of keys) {
     const names = report[`${key}`];
     for (const employee of names) {
