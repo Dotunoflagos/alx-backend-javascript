898f161d1e5c097b819e4f29e81170679fb7d5e4
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-07-27 23:55:14 +0100

    101-iterateThroughObject.js2

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   let string = '';
-  for (const employee of reportWithIterator) {
+  for (let employee of reportWithIterator) {
     string += employee;
     if (reportWithIterator.indexOf(employee) < reportWithIterator.length - 1) {
       string += ' | ';
