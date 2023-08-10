86e593fb4d7d81737ccd121073842b0dbf3b9872
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-08-10 01:17:03 +0100

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,7 @@ export default function getStudentIdsSum(list) {
   if (list instanceof Array) {
     return list.reduce(
       (prevStudent, curStudent) => ({ id: prevStudent.id + curStudent.id }),
-      { id: 0 }
+      { id: 0 },
     ).id;
   }
   return 0;
