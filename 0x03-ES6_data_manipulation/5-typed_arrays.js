cefe00fb784d9adb9fcad9163e9e016a7737a15f
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-08-12 00:25:53 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -8,5 +8,5 @@ export default function createInt8TypedArray(length, position, value) {
   const int8Array = new Int8Array(buffer);
   int8Array[position] = value;
 
-  return buffer;
+  return int8Array;
 }
