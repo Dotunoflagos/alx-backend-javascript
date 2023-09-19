d0dcc280f05de5883a04053a36b68ff27a205aae
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-19 06:19:40 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,18 +2,14 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  if (input.trim() === '') {
-    console.log("Your name is: [No input provided]");
-  } else {
-    console.log(`Your name is: ${input}`);
-  }
+  console.log(`Your name is: ${input}`);
 
-  console.log("This important software is now closing");
+  console.log('This important software is now closing');
   rl.close();
 });
