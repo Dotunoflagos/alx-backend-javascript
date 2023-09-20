6d9975978226b553b390387684590e1f9040e652
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-20 21:08:37 +0100

    2-read_file.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,11 +5,11 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
+  process.stdout.write(`Your name is: ${input}\n`);
 
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
   rl.close();
 });
