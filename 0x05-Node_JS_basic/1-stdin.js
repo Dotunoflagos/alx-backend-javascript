9f36e889d9cb4d6eceff58cced8c4049d6ee54ea
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-21 05:43:32 +0100

    2-read_file.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,10 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (input) => {
+process.stdin.on('readable', () => {
+  const input = process.stdin.read()
   process.stdout.write(`Your name is: ${input}\n`);
+});
 
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  rl.close();
 });
