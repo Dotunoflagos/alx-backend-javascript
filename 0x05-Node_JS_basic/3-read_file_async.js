c6b00977348efc2c2d10ece2093df867e2e47831
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-21 12:02:46 +0100

    3-read_file_async.js-fixed

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,31 +2,36 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    // Specify the file path you want to read
+    // Specify the file path yyou want to read
     const filePath = path;
 
     // Read the file synchronously
-    const fileContent = fs.readFile(filePath, 'utf-8')
+    fs.readFile(filePath, 'utf-8', process)
+
+    function process(err, fileContent) {
+      // count students
+      const studentArray = fileContent
       .toString('utf-8')
-      .trim();
-    // count students
-    const studentArray = fileContent.split('\n').slice(1);
-    // Process the file content as needed
-    console.log(`Number of students: ${studentArray.length}`);
-    const field = Array.from(
-      new Set(studentArray.map((st) => st.split(',')[3])),
-    );
+      .trim()
+      .split('\n')
+      .slice(1);
+      // Process the file content as needed
+      console.log(`Number of students: ${studentArray.length}`);
+      const field = Array.from(
+        new Set(studentArray.map((st) => st.split(',')[3])),
+      );
 
-    field.forEach((fl) => {
-      const number = studentArray.filter((st) => st.split(',')[3] === fl).length;
+      field.forEach((fl) => {
+        const number = studentArray.filter((st) => st.split(',')[3] === fl).length;
 
-      const list = studentArray.filter((st) => st.split(',')[3] === fl).map((st) => st.split(',')[0]);
-      console.log(`Number of students in ${fl}: ${number}. List: ${list.join(', ')}`);
-    });
+        const list = studentArray.filter((st) => st.split(',')[3] === fl).map((st) => st.split(',')[0]);
+        console.log(`Number of students in ${fl}: ${number}. List: ${list.join(', ')}`);
+      });
+    }
 
-    resolve(true);
   } catch (error) {
     // Handle any errors that occur during the file read
+    console.log(error)
     throw new Error('Cannot load the database');
   }
 }
