2e6b4d02bee33234db191bcb61f25749381afb4c
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-21 06:08:49 +0100

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const input = process.stdin.read()
+  const input = process.stdin.read();
   process.stdout.write(`Your name is: ${input}\n`);
 });
 
