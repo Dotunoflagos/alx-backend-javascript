338d837a481e67b7a3cf0dbe7c67eeeb7a8db48e
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-21 12:04:23 +0100

    3-read_file_async.js-fixed

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,36 @@
 const fs = require('fs');
 
 function countStudents(path) {
+  function process(err, fileContent) {
+    // count students
+    const studentArray = fileContent
+      .toString('utf-8')
+      .trim()
+      .split('\n')
+      .slice(1);
+    // Process the file content as needed
+    console.log(`Number of students: ${studentArray.length}`);
+    const field = Array.from(
+      new Set(studentArray.map((st) => st.split(',')[3])),
+    );
+
+    field.forEach((fl) => {
+      const number = studentArray.filter((st) => st.split(',')[3] === fl).length;
+
+      const list = studentArray.filter((st) => st.split(',')[3] === fl).map((st) => st.split(',')[0]);
+      console.log(`Number of students in ${fl}: ${number}. List: ${list.join(', ')}`);
+    });
+  }
+
   try {
     // Specify the file path yyou want to read
     const filePath = path;
 
     // Read the file synchronously
-    fs.readFile(filePath, 'utf-8', process)
-
-    function process(err, fileContent) {
-      // count students
-      const studentArray = fileContent
-      .toString('utf-8')
-      .trim()
-      .split('\n')
-      .slice(1);
-      // Process the file content as needed
-      console.log(`Number of students: ${studentArray.length}`);
-      const field = Array.from(
-        new Set(studentArray.map((st) => st.split(',')[3])),
-      );
-
-      field.forEach((fl) => {
-        const number = studentArray.filter((st) => st.split(',')[3] === fl).length;
-
-        const list = studentArray.filter((st) => st.split(',')[3] === fl).map((st) => st.split(',')[0]);
-        console.log(`Number of students in ${fl}: ${number}. List: ${list.join(', ')}`);
-      });
-    }
-
+    fs.readFile(filePath, 'utf-8', process);
   } catch (error) {
     // Handle any errors that occur during the file read
-    console.log(error)
+    console.log(error);
     throw new Error('Cannot load the database');
   }
 }
