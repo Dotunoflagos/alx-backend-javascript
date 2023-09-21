59935c58870e17b96f3aa8a51b80320dcda86e51
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-21 12:11:06 +0100

    3-read_file_async.js-fixed

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,28 +3,26 @@ const fs = require('fs');
 function countStudents(path) {
   function process(err, fileContent) {
     // count students
-    try {
-      const studentArray = fileContent
-        .toString('utf-8')
-        .trim()
-        .split('\n')
-        .slice(1);
-      // Process the file content as needed
-      console.log(`Number of students: ${studentArray.length}`);
-      const field = Array.from(
-        new Set(studentArray.map((st) => st.split(',')[3])),
-      );
-
-      field.forEach((fl) => {
-        const number = studentArray.filter((st) => st.split(',')[3] === fl).length;
-
-        const list = studentArray.filter((st) => st.split(',')[3] === fl).map((st) => st.split(',')[0]);
-        console.log(`Number of students in ${fl}: ${number}. List: ${list.join(', ')}`);
-      });
-    } catch (error) {
-      // Handle any errors that occur during the file read
-      throw new Error('Cannot load the database');
+    const studentArray = fileContent
+      .toString('utf-8')
+      .trim()
+      .split('\n')
+      .slice(1);
+    if (err) {
+      reject(new Error('Cannot load the database'));
     }
+    // Process the file content as needed
+    console.log(`Number of students: ${studentArray.length}`);
+    const field = Array.from(
+      new Set(studentArray.map((st) => st.split(',')[3])),
+    );
+
+    field.forEach((fl) => {
+      const number = studentArray.filter((st) => st.split(',')[3] === fl).length;
+
+      const list = studentArray.filter((st) => st.split(',')[3] === fl).map((st) => st.split(',')[0]);
+      console.log(`Number of students in ${fl}: ${number}. List: ${list.join(', ')}`);
+    });
   }
 
   try {
